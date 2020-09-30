dd13947a0b0ff20a5e48baae5b8f2acd82834da7
bdbmax <maximebdeblois@gmail.com>
2020-09-30 15:37:57 +0200

    addition of key_date_regulations

--- R/01_startup.R
+++ R/01_startup.R
@@ -29,7 +29,8 @@ library(patchwork)
 # Set global variables ----------------------------------------------------
 
 if (Sys.info()["sysname"] != "Windows") {plan(multiprocess)}
-key_date <- as.Date("2020-03-14")
+key_date_covid <- as.Date("2020-03-14")
+key_date_regulations <- as.Date("2018-08-23")
 LTM_start_date <- as.Date("2019-01-01")
 LTM_end_date <- as.Date("2019-12-31")
 col_palette <- 
