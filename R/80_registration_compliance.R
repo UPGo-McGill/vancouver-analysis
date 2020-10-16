bebd509d97312182233861977947afc4ba65058d
bdbmax <maximebdeblois@gmail.com>
2020-10-15 20:07:53 -0400

    non-conformiry lucrativity?

--- R/80_registration_compliance.R
+++ R/80_registration_compliance.R
@@ -76,7 +76,7 @@ conformity_status %>%
   xlab("")+
   ylab("Number of listings")+
   guides(x = guide_axis(angle = 10))+
-  scale_fill_manual(name = "Registration conformity", values = col_palette[c(1, 2, 3, 4)])+
+  scale_fill_manual(name = "Registration conformity", values = col_palette[c(4,2,3,6)])+
   theme_minimal()
 
 # percentage of non-conform active listings
@@ -107,7 +107,7 @@ conformity_status %>%
   xlab("")+
   ylab("Number of listings")+
   guides(x = guide_axis(angle = 10))+
-  scale_fill_manual(name = "Registration conformity", values = col_palette[c(1, 2, 3, 4)])+
+  scale_fill_manual(name = "Registration conformity", values = col_palette[c(4, 2, 3, 6)])+
   theme_minimal()
 
 
@@ -143,11 +143,131 @@ LA %>%
   filter(registration_analyzed == "Non-conform") %>%
   ggplot()+
   geom_sf(aes(fill = non_conform_per), colour = "white") +
-  scale_fill_gradientn(colors = col_palette[c(6, 1)], na.value = "grey80",
+  scale_fill_gradientn(colors = col_palette[c(6, 3, 1)], na.value = "grey80",
                        # limits = c(0, 0.5), oob = scales::squish, 
                        labels = scales::percent)  +
   guides(fill = guide_colourbar(title = "Non-conform listings in %",
                                 title.vjust = 0.5)) + 
   theme_void()+
   geom_sf_text(aes(label = n), colour = "black")+
-  ggtitle("Percentage of non-conform listings (gradient) and\nnumber of non-conform listings (numerical) per local area")
+  ggtitle("      Percentage of non-conform listings (gradient) and\n      number of non-conform listings (numerical) per local area")
+
+
+### Non-conformity lucrativity ----------------------------------------------
+property_lucrativity <- 
+conformity_status %>% 
+  filter(registration_analyzed != "Inactive listing",
+         active >= max(active, na.rm = T) - days(30)) %>% 
+  # mutate(registration_analyzed = ifelse(registration_analyzed != "Conform",
+  #                                       "Non-conform", registration_analyzed)) %>%
+  filter(listing_type == "Entire home/apt")
+
+# revenue per listing since COVID
+revenue_covid <- 
+daily %>% 
+  filter(status == "R", date >= key_date_covid) %>%
+  group_by(property_ID) %>% 
+  summarize(revenue = sum(price), first_reserved = min(date)) %>% 
+  mutate(revenue = revenue / as.numeric(max(daily$date) - first_reserved)) %>% 
+  filter(revenue != Inf)
+
+property_lucrativity %>% 
+  st_drop_geometry() %>% 
+  left_join(revenue_covid, by = "property_ID") %>% 
+  group_by(registration_analyzed) %>% 
+  summarize(`Revenue per day` = round(mean(revenue, na.rm = T), digit = 2)) %>%
+  mutate(label = str_glue("{`Revenue per day`}$")) %>% 
+  ggplot()+
+  geom_bar(stat = "identity", aes(registration_analyzed, `Revenue per day`, fill = registration_analyzed))+
+  xlab("")+
+  ylab("Average daily revenue")+
+  ggtitle("Average daily revenue since COVID-19, per registration conformity")+
+  guides(x = guide_axis(angle = 10))+
+  scale_fill_manual(name = "Registration conformity", values = col_palette[c(4, 2, 3, 6)])+
+  geom_text(aes(registration_analyzed, `Revenue per day`, label = label), vjust=1.6, color="white")+
+  theme_minimal()
+  
+  property_lucrativity %>% 
+    st_drop_geometry() %>% 
+    left_join(revenue_covid, by = "property_ID") %>% 
+    mutate(registration_analyzed = ifelse(registration_analyzed != "Conform",
+                                          "Non-conform", registration_analyzed)) %>% 
+    group_by(registration_analyzed) %>% 
+    summarize(`Revenue per day` = round(mean(revenue, na.rm = T), digit = 2)) %>% View
+    
+  
+# revenue per listing since regulations
+revenue_regulations <- 
+  daily %>% 
+  filter(status == "R", date >= key_date_regulations) %>%
+  group_by(property_ID) %>% 
+  summarize(revenue = sum(price), first_reserved = min(date)) %>% 
+  mutate(revenue = revenue / as.numeric(max(daily$date) - first_reserved)) %>% 
+  filter(revenue != Inf)
+
+property_lucrativity %>% 
+  st_drop_geometry() %>% 
+  left_join(revenue_regulations, by = "property_ID") %>%
+  group_by(registration_analyzed) %>% 
+  summarize(`Revenue per day` = round(mean(revenue, na.rm = T), digit = 2)) %>%
+  mutate(label = str_glue("{`Revenue per day`}$")) %>% 
+  ggplot()+
+  geom_bar(stat = "identity", aes(registration_analyzed, `Revenue per day`, fill = registration_analyzed))+
+  xlab("")+
+  ylab("Average daily revenue ($)")+
+  ggtitle("Average daily revenue since regulation, per registration conformity")+
+  guides(x = guide_axis(angle = 10))+
+  scale_fill_manual(name = "Registration conformity", values = col_palette[c(4, 2, 3, 6)])+
+  geom_text(aes(registration_analyzed, `Revenue per day`, label = label), vjust=1.6, color="white")+
+  theme_minimal()
+
+# average nights available
+available_covid <- 
+  daily %>% 
+  filter(status == "A", date >= key_date_covid) %>%
+  group_by(property_ID) %>% 
+  summarize(nights_available = n(), first_available = min(date)) %>% 
+  mutate(nights_available = round(nights_available / as.numeric(max(daily$date) - first_available), digit=2)) %>% 
+  filter(nights_available != Inf)
+
+property_lucrativity %>% 
+  st_drop_geometry() %>% 
+  left_join(available_covid, by = "property_ID") %>% 
+  group_by(registration_analyzed) %>% 
+  summarize(nights_available = round(mean(nights_available, na.rm = T), digit = 2)) %>%
+  mutate(label = str_glue("{nights_available}%")) %>%
+  ggplot()+
+  geom_bar(stat = "identity", aes(registration_analyzed, nights_available, fill = registration_analyzed))+
+  xlab("")+
+  ylab("Average nights available (%)")+
+  ggtitle("Percentage of nights available since COVID-19, per registration conformity")+
+  guides(x = guide_axis(angle = 10))+
+  scale_fill_manual(name = "Registration conformity", values = col_palette[c(4, 2, 3, 6)])+
+  geom_text(aes(registration_analyzed, nights_available, label = label), vjust=1.6, color="white")+
+  theme_minimal()
+
+# average nights reserved
+reserved_covid <- 
+  daily %>% 
+  filter(status == "R", date >= key_date_covid) %>%
+  group_by(property_ID) %>% 
+  summarize(nights_reserved = n(), first_reserved = min(date)) %>% 
+  mutate(nights_reserved = round(nights_reserved / as.numeric(max(daily$date) - first_reserved), digit=2)) %>% 
+  filter(nights_reserved != Inf)
+
+property_lucrativity %>% 
+  st_drop_geometry() %>% 
+  left_join(reserved_covid, by = "property_ID") %>% 
+  group_by(registration_analyzed) %>% 
+  summarize(nights_reserved = round(mean(nights_reserved, na.rm = T), digit = 2)) %>%
+  mutate(label = str_glue("{nights_reserved}%")) %>%
+  ggplot()+
+  geom_bar(stat = "identity", aes(registration_analyzed, nights_reserved, fill = registration_analyzed))+
+  xlab("")+
+  ylab("Average nights available (%)")+
+  ggtitle("Percentage of nights reserved since COVID-19, per registration conformity")+
+  guides(x = guide_axis(angle = 10))+
+  scale_fill_manual(name = "Registration conformity", values = col_palette[c(4, 2, 3, 6)])+
+  geom_text(aes(registration_analyzed, nights_reserved, label = label), vjust=1.6, color="white")+
+  theme_minimal()
+
