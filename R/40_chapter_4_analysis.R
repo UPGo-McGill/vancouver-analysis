b41287d728a4f1430b0c66b1dca4bf70ac60277a
Cloe St-Hilaire <cloe.st-hilaire@mail.mcgill.ca>
2020-10-23 17:06:08 -0400

    Updated Vancouver's COVID Chapter Script

--- R/40_chapter_4_analysis.R
+++ R/40_chapter_4_analysis.R
@@ -131,13 +131,13 @@ average_prices <-
 
 # Reservations and prices collapsed during COVID-19 -----------------------
 
-#' There were 12.1% [1] more nights reserved in Montreal STRs in 2019 than 
-#' there were in 2018—peaking at nearly 9,500 [2] nightly STR reservations in 
+#' There were 4.5% [1] more nights reserved in Vancouver STRs in 2019 than 
+#' there were in 2018—peaking at nearly 4,000 [2] nightly STR reservations in 
 #' the summer of 2019.... In March 2020, however, when reserved nights should 
 #' have been steadily increasing en route to the summer peak, they instead 
 #' collapsed in the face of COVID-19. While total reserved nights from January 
-#' to February 2020 increased at a rapid 45.3% [3] compared to 2019, reserved 
-#' nights from March to August 2020 decreased 70.7% [4] compared to the previous 
+#' to February 2020 increased at a rapid 76.7% [3] compared to 2019, reserved 
+#' nights from March to August 2020 decreased 63.2% [4] compared to the previous 
 #' year.
 
 #' [1] YOY growth in reservations, 2018-2019
@@ -169,16 +169,16 @@ daily %>%
   count(date_2020 = date >= LTM_start_date + years(1)) %>% 
   summarize((n[2] - n[1]) / n[1])
 
-#' On July 31, 2020, fewer than 1,900 [1] STRs were reserved in Montreal. But 
+#' On July 31, 2020, fewer than 1,275 [1] STRs were reserved in Vancouver. But 
 #' the trajectory of STR activity established prior to the pandemic, combined 
 #' with the fact that bookings normally increase rapidly through the spring and 
-#' summer, suggests that, in the absence of the pandemic, Montreal would have 
-#' been expected to receive 8,700 [1] reservations instead. The COVID-19 
-#' pandemic, therefore, depressed STR activity by 78.3% [1], or 6,810 
+#' summer, suggests that, in the absence of the pandemic, Vancouver would have 
+#' been expected to receive 4,383 [1] reservations instead. The COVID-19 
+#' pandemic, therefore, depressed STR activity by 70.9% [1], or 3,108 
 #' [1] reservations, on that date. In total, from March through July 2020, we 
-#' estimate that there have been 680,600 [2] fewer STR nights reserved than 
-#' would normally have been expected to occur. The 317,500 [2] total nights 
-#' reserved in this time period is only 31.8% [2] of the 1.0 million [2] total 
+#' estimate that there have been 355,890 [2] fewer STR nights reserved than 
+#' would normally have been expected to occur. The 187,723 [2] total nights 
+#' reserved in this time period is only 34.5% [2] of the 543,613 million [2] total 
 #' that would represent the previous growth trend.
 
 #' [1] Actual and expected reservations on 2020-07-31
@@ -193,9 +193,9 @@ reservations %>%
   mutate(dif = trend - n, pct = n / trend)
   
 #' Throughout the May-August period of 2020, nightly prices have been an average 
-#' of 22.2% [1] lower than expected. Spread across the 320,700 nights reserved 
+#' of 34.0% [1] lower than expected. Spread across the 187,723 nights reserved 
 #' during this period, this means that STR operators collectively earned 
-#' $9.0 million [2] less than they would have on their bookings in the absence 
+#' $11.3 million [2] less than they would have on their bookings in the absence 
 #' of the pandemic.
 
 #' [1] Average price difference
@@ -212,8 +212,8 @@ average_prices %>%
   tally(rev_dif)
 
 #' When the lower prices on reservations which did occur is combined with the 
-#' reservations which did not occur, our estimate is that Montreal’s STR hosts 
-#' lost a total of $106.0 million [1] in revenue between March and August 2020 
+#' reservations which did not occur, our estimate is that Vancouver’s STR hosts 
+#' lost a total of $86.2 million [1] in revenue between March and August 2020 
 #' because of the COVID-19 pandemic.
 
 #' [1] Total lost revenue
@@ -226,161 +226,16 @@ average_prices %>%
   tally(total_rev_dif)
 
 
-# Compliance with the Provincial STR ban ----------------------------------
-
-#' Between March 29 and June 25, there were a total of 34,980 [1] reservations 
-#' in the City, compared to 129,020 [2] for the same period in 2019 (a decrease 
-#' of 72.9% [3]). Only 380 [4] of these reservations were for longer than 30 
-#' days, which means that the remaining 34,600 [5] reservations in Montreal were 
-#' illegal. Additionally, these illegal reservations were distributed widely 
-#' among Montreal’s STR hosts. There were 6,120 [6] hosts with active listings 
-#' in the March 29 - June 25 period, and 4,360 [7] of these hosts received at 
-#' least one reservation. 4,330 [8] hosts received reservations of 30 days or 
-#' fewer, which means that 70.7% [9] of Montreal’s active STR hosts appear to 
-#' have violated the Provincial order.
-
-#' [1] Total reservations 2020: add unique res_ID count to compressed no-ID rows
-daily %>% 
-  filter(housing, status == "R", date >= "2020-03-29", date <= "2020-06-25") %>% 
-  filter(is.na(res_ID)) %>% 
-  select(1:12) %>% 
-  strr_compress(quiet = TRUE) %>% 
-  nrow() %>% 
-  `+`(daily %>% 
-        filter(housing, status == "R", date >= "2020-03-29", 
-               date <= "2020-06-25", !is.na(res_ID)) %>% 
-        count(res_ID) %>% 
-        nrow()) %>% 
-  round(-1)
-
-#' [2] Total reservations 2019
-daily %>% 
-  filter(housing, status == "R", date >= "2019-03-29", date <= "2019-06-25") %>% 
-  filter(is.na(res_ID)) %>% 
-  select(1:12) %>% 
-  strr_compress(quiet = TRUE) %>% 
-  nrow() %>% 
-  `+`(daily %>% 
-        filter(housing, status == "R", date >= "2019-03-29", 
-               date <= "2019-06-25", !is.na(res_ID)) %>% 
-        count(res_ID) %>% 
-        nrow()) %>% 
-  round(-1)
-
-#' [3] Change in reservations
-{1 - {daily %>% 
-    filter(housing, status == "R", date >= "2020-03-29", 
-           date <= "2020-06-25") %>% 
-    filter(is.na(res_ID)) %>% 
-    select(1:12) %>% 
-    strr_compress(quiet = TRUE) %>% 
-    nrow() %>% 
-    `+`(daily %>% 
-          filter(housing, status == "R", date >= "2020-03-29", 
-                 date <= "2020-06-25", !is.na(res_ID)) %>% 
-          count(res_ID) %>% 
-          nrow())} / 
-  {daily %>% 
-      filter(housing, status == "R", date >= "2019-03-29", 
-             date <= "2019-06-25") %>% 
-      filter(is.na(res_ID)) %>% 
-      select(1:12) %>% 
-      strr_compress(quiet = TRUE) %>% 
-      nrow() %>% 
-      `+`(daily %>% 
-            filter(housing, status == "R", date >= "2019-03-29", 
-                   date <= "2019-06-25", !is.na(res_ID)) %>% 
-            count(res_ID) %>% 
-            nrow())
-  }} %>% 
-  round(3)
-
-#' [4] Long reservations
-daily %>% 
-  filter(housing, status == "R", date >= "2020-03-01") %>% 
-  count(res_ID, sort = TRUE) %>% 
-  filter(n >= 31, !is.na(res_ID)) %>% 
-  nrow() %>% 
-  round(-1)
-
-#' [5] Short reservations
-daily %>% 
-  filter(housing, status == "R", date >= "2020-03-29", date <= "2020-06-25") %>% 
-  filter(is.na(res_ID)) %>% 
-  select(1:12) %>% 
-  strr_compress() %>% 
-  nrow() %>% 
-  `+`(daily %>% 
-        filter(housing, status == "R", date >= "2020-03-29", 
-               date <= "2020-06-25", !is.na(res_ID)) %>% 
-        count(res_ID) %>% 
-        nrow()) %>% 
-  `-`(daily %>% 
-        filter(housing, status == "R", date >= "2020-03-01") %>% 
-        count(res_ID, sort = TRUE) %>% 
-        filter(n >= 31, !is.na(res_ID)) %>% 
-        nrow()) %>% 
-  round(-1)
-
-#' [6] All active hosts
-daily %>% 
-  filter(housing, status %in% c("R", "A"), date >= "2020-03-29", 
-         date <= "2020-06-25", !is.na(host_ID)) %>% 
-  count(host_ID) %>% 
-  nrow() %>% 
-  round(-1)
-
-#' [7] Hosts with reservations
-daily %>% 
-  filter(housing, status == "R", date >= "2020-03-29", 
-         date <= "2020-06-25", !is.na(host_ID)) %>% 
-  count(host_ID) %>% 
-  nrow() %>% 
-  round(-1)
-
-#' [8] Hosts with short reservations
-daily %>% 
-  filter(housing, status == "R", date >= "2020-03-29", 
-         date <= "2020-06-25", !is.na(host_ID), 
-         (is.na(res_ID) | !res_ID %in% {
-           daily %>% 
-             filter(housing, status == "R", date >= "2020-03-01") %>% 
-             count(res_ID, sort = TRUE) %>% 
-             filter(n >= 31, !is.na(res_ID)) %>% 
-             pull(res_ID)})) %>% 
-  count(host_ID) %>% 
-  nrow() %>% 
-  round(-1)
-
-#' [9] Percentage of hosts with short reservations
-{{daily %>% 
-    filter(housing, status == "R", date >= "2020-03-29", 
-           date <= "2020-06-25", !is.na(host_ID), 
-           (is.na(res_ID) | !res_ID %in% {
-             daily %>% 
-               filter(housing, status == "R", date >= "2020-03-01") %>% 
-               count(res_ID, sort = TRUE) %>% 
-               filter(n >= 31, !is.na(res_ID)) %>% 
-               pull(res_ID)})) %>% 
-    count(host_ID) %>% 
-    nrow()} / {daily %>% 
-        filter(housing, status %in% c("R", "A"), date >= "2020-03-29", 
-               date <= "2020-06-25", !is.na(host_ID)) %>% 
-        count(host_ID) %>% 
-        nrow()}} %>% 
-  round(3)
-
-
 # COVID’s impact on frequently rented entire-home listings ----------------
 
-#' According to the model, the number of housing units in Montreal lost due to 
-#' commercial STRs reached its all-time peak (5,860 [1]) at the beginning of 
-#' 2020. Most of these (5,580 [1]) were FREH listings, with the remainder
-#' (290 [1]) being ghost hostels—clusters of private-room listings operated 
+#' According to the model, the number of housing units in Vancouver lost due to 
+#' commercial STRs reached a peak level since 2017 and the regulations (2,640 [1])
+#' at the beginning of 2020. Most of these (2,430 [1]) were FREH listings, with the remainder
+#' (210 [1]) being ghost hostels—clusters of private-room listings operated 
 #' out of a single housing unit. As of July 2020, the number of FREH listings 
 #' had dropped to its lowest amount since we began tracking it in 2016, with 
-#' just 1,750 [2] listings displaying availability and reservations consistent 
-#' with historical patterns of full-time STR activity in Montreal.
+#' just 830 [2] listings displaying availability and reservations consistent 
+#' with historical patterns of full-time STR activity in Vancouver.
 
 #' [1] Peak housing loss
 GH_total <- 
@@ -395,6 +250,7 @@ daily %>%
   summarize(FREH = sum(FREH_3)) %>% 
   left_join(GH_total) %>% 
   mutate(housing_loss = FREH + GH_units) %>% 
+  filter(date>=key_date_regulations) %>% 
   filter(housing_loss == max(housing_loss, na.rm = TRUE)) %>% 
   mutate(across(-date, round, -1))
 
@@ -407,14 +263,14 @@ daily %>%
   filter(date == "2020-07-01") %>% 
   mutate(across(-date, round, -1))
 
-#' There were 5,900 [1] listings which we consider likely to have been FREH in 
-#' either or both of January and February 2020. Of these listings, 1,940 [2] 
+#' There were 2,700 [1] listings which we consider likely to have been FREH in 
+#' either or both of January and February 2020. Of these listings, 850 [2] 
 #' were no longer listed on Airbnb or VRBO as of July 31, 2020. This is 
-#' 32.8% [2] of these listings—twice as high as the 16.6% [3] of listings which 
+#' 31.4% [2] of these listings—twice as high as the 17.6% [3] of listings which 
 #' were FREH in either January or February 2019 and were no longer listed on the 
-#' STR platforms by the end of July 2019. In total, 36.1% [4] of non-FREH 
+#' STR platforms by the end of July 2019. In total, 42.9% [4] of non-FREH 
 #' listings active in January or February 2020 were deactivated by the end of 
-#' July 2020, while the corresponding figure last year was 32.7% [5].
+#' July 2020, while the corresponding figure last year was 27.4% [5].
 
 #' [1] FREH in either Jan or Feb 2020
 FREH_in_jan_feb <- 
@@ -464,13 +320,13 @@ property %>%
       unique()}, !property_ID %in% FREH_in_jan_feb_2019) %>% 
   summarize(mean(scraped <= "2019-07-31"))
 
-#' Of the 3,970 [1] FREH listings which remained listed throughout March - July,
-#' 1,340 [2] (33.8% [3]) were blocked (i.e. not available for reservations) for 
-#' all of the month of July, and 2,060 [4] (51.8% [5]) were blocked for a 
+#' Of the 1,850 [1] FREH listings which remained listed throughout March - July,
+#' 500 [2] (26.8% [3]) were blocked (i.e. not available for reservations) for 
+#' all of the month of July, and 880 [4] (47.4% [5]) were blocked for a 
 #' majority of the month. This is extremely rare behaviour for a dedicated STR 
 #' listing, since the summer is usually the busiest season for tourist
-#' accommodations in Montreal. In 2019, only 11.9% [6] of listings which were 
-#' FREH in January or February were blocked for all of July, and only 20.3% [7] 
+#' accommodations in Vancouver. In 2019, only 9.5% [6] of listings which were 
+#' FREH in January or February were blocked for all of July, and only 18.5% [7] 
 #' were blocked for a majority of the month.
 
 #' [1] Jan-Feb FREH units still active at end of July 2020
@@ -561,7 +417,7 @@ daily %>%
           nrow())} %>% 
   round(3)
 
-#' For example, in the month of February 2020, 63.7% [1] of all reserved nights
+#' For example, in the month of February 2020, 58.4% [1] of all reserved nights
 #' were booked in these FREH properties. 
 
 #' [1] Percentage of reserved nights in FREH properties in Feb 2020
